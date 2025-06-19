dd1c7b22afd9fc6f82cffc53eedf201889522965
Fernante <fernandocorreo79@gmail.com>
2025-06-19 13:01:58 +0100

    Primer

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+$encabezado = "<html><head><title>Ejercicios</title></head><body>";
+$pie ="</body></html>";
+$encabezado;
+
+// 1. Mostrar un mensaje de saludo usando una variable.
+$men = "<h1 style= text-align:center;>¡Hola esto es un mensaje de bienvenida!</h1>";
+echo"$men";
+
+
+
+$pie;
+?>
